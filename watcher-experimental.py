0c8e5d6006b902f77f8ff17f80c704f8b2ac0367
Gordon Watts <gordon@gordonandpaula.net>
2017-12-24 05:15:58 +0000

    fix coding errors

--- watcher-experimental.py
+++ watcher-experimental.py
@@ -14,15 +14,15 @@ def rc_time (pin):
 	count = 0
   
 	#Output on the pin for 
-	GPIO.setup(pin_to_circuit, GPIO.OUT)
-	GPIO.output(pin_to_circuit, GPIO.LOW)
-	time.sleep(0.1)
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, GPIO.LOW)
+	sleep(0.1)
 
 	#Change the pin back to input
-	GPIO.setup(pin_to_circuit, GPIO.IN)
+	GPIO.setup(pin, GPIO.IN)
   
 	#Count until the pin goes high
-	while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+	while (GPIO.input(pin) == GPIO.LOW):
 		count += 1
 
 	return count
@@ -31,7 +31,7 @@ def rc_time (pin):
 try:
     # Main loop
     while True:
-        print rc_time(pin_to_circuit)
+        print rc_time(sensor_1_pin)
 except KeyboardInterrupt:
     pass
 finally:
